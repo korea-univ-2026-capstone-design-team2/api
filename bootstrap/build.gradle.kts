228de2602b298c4d01b4dd39e998bc806de4cda4
liverforpresent <dailynsteady@gmail.com>
2026-04-22 22:43:50 +0900

    chore: swagger 관련 dependency 설정

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -7,11 +7,19 @@ dependencies {
     implementation(project(":shared:kernel"))
     implementation(project(":shared:infrastructure"))
 
+    // Question
+    implementation(project(":question:application"))
+    implementation(project(":question:adapter:web"))
+    implementation(project(":question:adapter:persistence"))
+
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     compileOnly("org.springframework.boot:spring-boot-configuration-processor")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
 
     // OAuth2
     implementation ("org.springframework.boot:spring-boot-starter-oauth2-client")
