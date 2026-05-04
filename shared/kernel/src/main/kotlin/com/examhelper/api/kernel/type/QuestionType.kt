40eb93728f3d08e4fa1ec3dc3d809bbe18c4a880
liverforpresent <dailynsteady@gmail.com>
2026-05-05 02:47:28 +0900

    fix(generation): QuestionType, Subject 구체화

--- shared/kernel/src/main/kotlin/com/examhelper/api/kernel/type/QuestionType.kt
+++ shared/kernel/src/main/kotlin/com/examhelper/api/kernel/type/QuestionType.kt
@@ -1,7 +1,28 @@
 package com.examhelper.api.kernel.type
 
-enum class QuestionType {
-    READING,                 // 독해형
-    LOGIC_PUZZLE,            // 논리퀴즈
-    ARGUMENTATION,           // 논증형
+enum class QuestionType(val korean: String) {
+    READING("독해") {
+        override fun compatibleSubTypes() = listOf(
+            QuestionSubType.MATCH,
+            QuestionSubType.KNOWABLE,
+            QuestionSubType.CONTEXT_CORRECTION,
+            QuestionSubType.BLANK_FILLING,
+        )
+    },
+    LOGIC_PUZZLE("논리퀴즈") {
+        override fun compatibleSubTypes() = listOf(
+            QuestionSubType.MATCH,
+            QuestionSubType.KNOWABLE,
+        )
+    },
+    ARGUMENTATION("논증") {
+        override fun compatibleSubTypes() = listOf(
+            QuestionSubType.CORE_ARGUMENT,
+            QuestionSubType.INFERENCE,
+            QuestionSubType.ARGUMENT_ANALYSIS,
+            QuestionSubType.STRENGTHEN_WEAKEN,
+        )
+    };
+
+    abstract fun compatibleSubTypes(): List<QuestionSubType>
 }
