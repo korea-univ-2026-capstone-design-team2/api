7abc1a2d9455170f3a14428e72ee8bceda9a9e4c
Rootsquare <3456monster@naver.com>
2026-05-14 14:24:53 +0900

    Initial
    Initial structure

--- member/src/main/kotlin/com/examhelper/api/member/Member.kt
+++ member/src/main/kotlin/com/examhelper/api/member/Member.kt
@@ -1,5 +1,15 @@
 package com.examhelper.api.member
 
-class Member {
+import com.examhelper.api.kernel.identifier.MemberId
+import java.time.Instant
+
+class Member private constructor(
+    id: MemberId,
+    nickname: String,
+    email: String,
+    authType: String, // 로그인 타입. ENUM 으로 하는게 좋을까요? 
+    val createdAt: Instant,
+    updatedAt: Instant
+    ){
     // 모듈 구조 유지용
 }
