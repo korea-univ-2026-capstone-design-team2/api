eb0a85a6ea8ce1b35a05358fe939d337e1606802
liveforpresent <dailynsteady@gmail.com>
2026-06-01 19:43:31 +0900

    feat(question): Question 해설지 목록 조회 로직 구현

---
+++ question/application/src/main/kotlin/com/examhelper/api/question/application/GetQuestionReviewsService.kt
@@ -0,0 +1,15 @@
+package com.examhelper.api.question.application
+
+import com.examhelper.api.question.port.inbound.GetQuestionReviewsUseCase
+import com.examhelper.api.question.port.inbound.query.GetQuestionReviewsQuery
+import com.examhelper.api.question.port.inbound.view.QuestionReviewView
+import com.examhelper.api.question.port.outbound.QuestionReader
+import org.springframework.stereotype.Service
+
+@Service
+class GetQuestionReviewsService(
+    private val questionReader: QuestionReader,
+) : GetQuestionReviewsUseCase {
+    override fun execute(query: GetQuestionReviewsQuery): List<QuestionReviewView> =
+        questionReader.findReviewsByIds(query.questionIds)
+}
