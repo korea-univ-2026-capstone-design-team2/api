36a950e002d6c27929cab35004a13659ba0507d2
Rootsquare <3456monster@naver.com>
2026-06-06 02:42:16 +0900

    feat(member) : 멤버 등록 골자 완성
    /member/register 로 이제 등록을 할 수 있도록 구조를 수정했습니다.

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -39,9 +39,6 @@ dependencies {
     // Auth
     implementation(project(":auth"))
 
-    // Member
-    implementation(project(":member"))
-
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
