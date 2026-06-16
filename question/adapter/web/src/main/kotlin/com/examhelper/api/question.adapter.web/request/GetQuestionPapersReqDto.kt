1aa94c87d6f64afdd884eacab8c81e338dd766d9
liveforpresent <dailynsteady@gmail.com>
2026-06-16 19:45:37 +0900

    feat(exam_attempt): ExamAttemptSubmittedEvent(시험 제출 이벤트) 처리 채널 변경(SpringApplicationEvent -> Kafka)

--- question/adapter/web/src/main/kotlin/com/examhelper/api/question.adapter.web/request/GetQuestionPapersReqDto.kt
+++ question/adapter/web/src/main/kotlin/com/examhelper/api/question.adapter.web/request/GetQuestionPapersReqDto.kt
@@ -3,8 +3,7 @@ package com.examhelper.api.question.adapter.web.request
 import com.examhelper.api.question.port.inbound.query.GetQuestionPapersQuery
 
 data class GetQuestionPapersReqDto(
-    val questionIds: List<String>,
-    val memberId: Long
+    val questionIds: List<String>
 ) {
     fun toQuery(memberId: Long): GetQuestionPapersQuery = GetQuestionPapersQuery(
         questionIds.map { it.toLong() },
