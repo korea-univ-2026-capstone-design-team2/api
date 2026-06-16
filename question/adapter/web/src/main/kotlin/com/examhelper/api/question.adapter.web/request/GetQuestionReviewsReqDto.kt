7a45ff87263b8525b39511e4a4756737880617be
liveforpresent <dailynsteady@gmail.com>
2026-06-16 20:55:36 +0900

    fix(question): memberId를 dto가 아닌 AuthenticationPrincipal을 이용하도록 수정

--- question/adapter/web/src/main/kotlin/com/examhelper/api/question.adapter.web/request/GetQuestionReviewsReqDto.kt
+++ question/adapter/web/src/main/kotlin/com/examhelper/api/question.adapter.web/request/GetQuestionReviewsReqDto.kt
@@ -3,8 +3,7 @@ package com.examhelper.api.question.adapter.web.request
 import com.examhelper.api.question.port.inbound.query.GetQuestionReviewsQuery
 
 data class GetQuestionReviewsReqDto(
-    val questionIds: List<String>,
-    val memberId: Long
+    val questionIds: List<String>
 ) {
     fun toQuery(memberId: Long): GetQuestionReviewsQuery = GetQuestionReviewsQuery(
         questionIds.map { it.toLong() },
